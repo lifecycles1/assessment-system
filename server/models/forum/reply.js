c927ae5e4b9a17611c88a6d165a75edc4838badb
lifecycles1 <raichev89@gmail.com>
2024-01-22 16:31:57 +0000

    db query optimization

--- server/models/forum/reply.js
+++ server/models/forum/reply.js
@@ -15,28 +15,37 @@ const replySchema = new mongoose.Schema({
 replySchema.methods.toggleLike = async function (userId) {
   const likeIndex = this.likes.indexOf(userId);
   if (likeIndex !== -1) {
-    // User has already liked, remove the like
     this.likes.splice(likeIndex, 1);
-    // Decrement the user's likesGiven count
-    await User.updateOne({ _id: userId }, { $inc: { likesGiven: -1 } });
-    // Decrement the reply's creator's likesReceived count
-    await User.updateOne({ _id: this.creator }, { $inc: { likesReceived: -1 } });
   } else {
-    // User hasn't liked, add a new like
     this.likes.push(userId);
-    // Increment the user's likesGiven count
-    await User.updateOne({ _id: userId }, { $inc: { likesGiven: 1 } });
-    // Increment the reply's creator's likesReceived count
-    await User.updateOne({ _id: this.creator }, { $inc: { likesReceived: 1 } });
   }
   await this.save();
+
+  await User.bulkWrite([
+    {
+      updateOne: {
+        filter: { _id: userId },
+        update: { $inc: { likesGiven: likeIndex !== -1 ? -1 : 1 } },
+      },
+    },
+    {
+      updateOne: {
+        filter: { _id: this.creator },
+        update: { $inc: { likesReceived: likeIndex !== -1 ? -1 : 1 } },
+      },
+    },
+  ]);
 
   const Topic = require("./topic");
   const topic = await Topic.findById(this.parentTopicId);
   const replyIndex = topic.replies.findIndex((reply) => reply._id.equals(this._id));
-  const topicLikeIndex = topic.replies[replyIndex].likes.indexOf(userId);
-  if (topicLikeIndex !== -1) {
-    topic.replies[replyIndex].likes.splice(topicLikeIndex, 1);
+  /**
+   * Index of the user's like in topic.reply.likes[]
+   * @type {number}
+   */
+  const userLikeIndex = topic.replies[replyIndex].likes.indexOf(userId);
+  if (userLikeIndex !== -1) {
+    topic.replies[replyIndex].likes.splice(userLikeIndex, 1);
   } else {
     topic.replies[replyIndex].likes.push(userId);
   }
