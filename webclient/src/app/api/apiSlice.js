9bc93c7451f5ea8142360f5b3c33de022afbdc7e
lifecycles1 <raichev89@gmail.com>
2024-08-15 15:42:18 +0100

    alles klaar

--- webclient/src/app/api/apiSlice.js
+++ webclient/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  baseUrl: "https://server-code-assessment.to6ko.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
